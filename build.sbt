b2447d1e3a34c26e094812584123532684a8bc0e
Aliaksandr Ivanou <aivanou@Aliaksandrs-MacBook-Pro.local>
2015-11-28 20:21:08 +0100

    added some algs

--- build.sbt
+++ build.sbt
@@ -4,3 +4,5 @@ version := "1.0"
 
 scalaVersion := "2.11.6"
 
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.2" % "test"
+
--- src/main/scala/Test.java
+++ src/main/scala/Test.java
@@ -4,14 +4,6 @@ import java.sql.Statement;
 import java.util.*;
 import java.util.List;
 
-public class Test
-{
+public class Test {
 
-
-    int v;
-    public int hashcode() { return (int)(v^5); }
-
-    public static void main(String [] args ) {
-        ResultSet
-    }
 }
